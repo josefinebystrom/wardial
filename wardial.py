603f640b9fcdf69d60e0265321111972b1a2eb82
Josefine Bystrom <bystromjosefine@gmail.com>
2023-05-13 19:49:44 -0700

    homework

--- wardial.py
+++ wardial.py
@@ -207,7 +207,7 @@ async def _wardial_async(hosts, max_connections=500, timeout=10, schema='http'):
         results = []
         for host in hosts:
             results.append(await is_server_at_host(session,host))
-        return results
+        return await asyncio.gather(*results)
 
 
 def wardial(hosts, **kwargs):
@@ -227,7 +227,10 @@ def wardial(hosts, **kwargs):
     # and use this event loop to call the `_wardial_async` function.
     # Ensure that all of the kwargs parameters get passed to `_wardial_async`.
     # You will have to do some post-processing of the results of this function to convert the output.
-    return []
+    loop = asyncio.new_event_loop()
+    results = loop.run_until_complete(_wardial_async(hosts, **kwargs))
+    loop.close()
+    return [host for host, result in zip(hosts, results) if result]
 
 if __name__=='__main__':
 
